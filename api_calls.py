ede2942cf82261ec294fa10018c1a6e996e92030
nick-esqueda <nick_esqueda@outlook.com>
2022-09-22 21:05:41 -0500

    added artist urls

--- api_calls.py
+++ api_calls.py
@@ -1,12 +1,21 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import json
+from artist_urls import URLS
 from dotenv import load_dotenv
 load_dotenv()
 
+# API SETUP #####################################
 auth_manager = SpotifyClientCredentials()
 spot = spotipy.Spotify(auth_manager=auth_manager)
 
-URL = "https://open.spotify.com/artist/2YZyLoL8N0Wb9xBt1NhZWg?si=Po23oXZdRRWBLqPMf70vxA"
-albums = spot.artist_albums(URL)
-print(json.dumps(albums, indent=2))
+
+# REQUESTS ######################################
+artist_URL = "https://open.spotify.com/artist/2YZyLoL8N0Wb9xBt1NhZWg?si=Po23oXZdRRWBLqPMf70vxA"
+artist = spot.artist(artist_URL)
+
+albums = spot.artist_albums(artist_URL)
+album_ids = [album["id"] for album in albums["items"]]
+
+print(album_ids)
+
---
+++ artist_urls.py
@@ -0,0 +1,23 @@
+URLS = [
+    "https://open.spotify.com/artist/65C6Unk7nhg2aCnVuAPMo8?si=ASf5DfypTkC3fzuJa9AoKA", # Animals As Leaders
+    "https://open.spotify.com/artist/6d24kC5fxHFOSEAmjQPPhc?si=dw_t0g69QveepoTdoon-ug", # Periphery
+    "https://open.spotify.com/artist/23ytwhG1pzX6DIVWRWvW1r?si=1wZ3ak4tQqel-KIH6s1RwQ", # TesseracT
+    "https://open.spotify.com/artist/0z4ODfFM8PGE0A9r0tZ75J?si=VETgrdO3Q-CUWNSFRvd55g", # Vildhjarta
+    "https://open.spotify.com/artist/6ncfp4E8TXGnp6nYwBUwwj?si=I5lGCaMqQoen8T1_3-_OyA", # Uneven Structure
+    "https://open.spotify.com/artist/3ggwAqZD3lyT2sbovlmfQY?si=4cp0qE9aQlqbmz2DBsqEcQ", # Meshuggah
+    "https://open.spotify.com/artist/3Gs10XJ4S4OEFrMRqZJcic?si=caQsC6pFTgOKbfk0vY45lQ", # Plini
+    "https://open.spotify.com/artist/4HgqjpBaWctBWVHafQIpRt?si=X5h0oYFjRsu2joMD3HMqlQ", # Born Of Osiris
+    "https://open.spotify.com/artist/0uNj4RxFjG0iVPlZS753en?si=7Q7st4o7T2ynE1Gn__87Fg", # After the Burial
+    "https://open.spotify.com/artist/2i7CQcVBh2K6uOR3CH09M1?si=BNWXpBzNS-e57HVrSLu0bw", # Veil of Maya
+    
+    "https://open.spotify.com/artist/4FXGRMSHh2JjHxVwS8dhH1?si=EPEe-mbbS9Wt198GtB2B6w", # Virtual Riot
+    "https://open.spotify.com/artist/5c3akKV3CUqAVOnGZqf4S3?si=554eXux4TPKYQn4mj1IdeQ", # Barely Alive
+    "https://open.spotify.com/artist/5oiSxivTCIoOk5UlMSwzWM?si=7ub72eaLS7OX0GhLbe867A", # TYNAN
+    "https://open.spotify.com/artist/2tKDLqWxsyXDmWoA4tHspF?si=F6Y89h8CR461Z8kF9EFqOQ", # Dr. Ozi
+    "https://open.spotify.com/artist/6hBrJJrcYoNhvLC6KaFR4b?si=YqGBFO1qQFakAFEuiyWnaA", # Eliminate
+    "https://open.spotify.com/artist/2u01kCKA5wDvvztuH8lyT0?si=9AMBMiROQ2S1zK_IClTk8w", # SVDDEN DEATH
+    "https://open.spotify.com/artist/4VT88y8BOxQjndfg6LS2Od?si=N1279B1XQmiNEMdrg-riyg", # Krischvn
+    "https://open.spotify.com/artist/7lm6PfYsdEvxXIqA5LaCA3?si=c0QggUYOS6O4TB3gPOSQEA", # Versa
+    "https://open.spotify.com/artist/3sS9RKjrDJMl7zpG3cWHk7?si=ssmJvLawRKCATfXQTkIwsQ", # Aweminus
+    "https://open.spotify.com/artist/3YLUvWzk9eBm1WrHFlZxM4?si=DdbVig84TCOYOpfhzkvOTw", # MARAUDA
+]
