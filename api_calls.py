85784e999128dd65f8a0947cc29c12ff4a981754
nick-esqueda <nick_esqueda@outlook.com>
2022-09-22 22:01:22 -0500

    created function to prune artist json responses, wrote code to populate a dict to be used as the artists dataframe

--- api_calls.py
+++ api_calls.py
@@ -11,11 +11,31 @@ spot = spotipy.Spotify(auth_manager=auth_manager)
 
 
 # REQUESTS ######################################
-artist_URL = "https://open.spotify.com/artist/2YZyLoL8N0Wb9xBt1NhZWg?si=Po23oXZdRRWBLqPMf70vxA"
-artist = spot.artist(artist_URL)
+def fetch_and_prune_artist(url: str) -> dict:
+    artist = spot.artist(url)
+    return {
+        "artist_id": artist["id"],
+        "artist_name": artist["name"],
+        "external_url": artist["external_urls"]["spotify"],
+        "genre": artist["genres"][0],
+        "image_url": artist["images"][0]["url"],
+        "followers": artist["followers"]["total"],
+        "popularity": artist["popularity"],
+        "type": artist["type"],
+        "artist_uri": artist["uri"]
+    }
 
-albums = spot.artist_albums(artist_URL)
-album_ids = [album["id"] for album in albums["items"]]
 
-print(album_ids)
+# take each artist URL and make a pruned version of the JSON response for each.
+pruned_artists = map(lambda url: fetch_and_prune_artist(url), URLS)
 
+# go through those pruned JSON responses and populate a dict with all the values.
+# NOTE: this will be used to create a pandas DataFrame later.
+artists = dict()
+for artist in pruned_artists:
+    for key in artist:
+        if key not in artists: # add the field name if not already there
+            artists[key] = []
+        artists[key].append(artist[key]) # add this artist's info to the list for each field
+        
+print(json.dumps(artists, indent=2))
