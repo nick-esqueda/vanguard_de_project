cfb77c97e6c2cddac58dd1aae66ff036a8f39ce4
nick-esqueda <nick_esqueda@outlook.com>
2022-09-26 17:12:59 -0500

    moved all transformation logic to functions for use in main() method

--- etl/transform.py
+++ etl/transform.py
@@ -8,61 +8,49 @@ tracks = pd.read_csv("data/tracks.csv")
 track_features = pd.read_csv("data/track_features.csv")
 
 
-# DROPPING UNNECESSARY COLUMNS
-artists.drop(columns="Unnamed: 0", inplace=True)
-albums.drop(columns="Unnamed: 0", inplace=True)
-tracks.drop(columns="Unnamed: 0", inplace=True)
-track_features.drop(columns="Unnamed: 0", inplace=True)
-
-
-# FIXING DUPLICATES
-albums.drop_duplicates(ignore_index=True, subset=["album_id"], inplace=True) # for duplicates from collaborations.
-albums.drop_duplicates(ignore_index=True, subset=["album_name", "total_tracks", "artist_id"], inplace=True) # for duplicates of the same album, different "spotify version".
-tracks.drop_duplicates(ignore_index=True, inplace=True)
-track_features.drop_duplicates(ignore_index=True, inplace=True)
-
-# remove the "appears_on" albums.
-albums_filt = albums["album_group"] != "appears_on"
-albums = albums[albums_filt]
-albums.sort_index(ignore_index=True, inplace=True)
-
-# remove all of the songs that aren't from the updated DataFrame of albums.
-tracks_filt = tracks["album_id"].isin(albums["album_id"])
-tracks = tracks[tracks_filt]
-tracks.sort_index(ignore_index=True, inplace=True)
-
-# grab all of the track_features that have a matching song in the updated tracks DataFrame.
-track_feat_filt = track_features["track_id"].isin(tracks["track_id"])
-track_features = track_features[track_feat_filt]
-track_features.sort_index(ignore_index=True, inplace=True)
-
-
-# print(albums)
-# print(tracks)
-# print(track_features)
-
-
-def clean_artists(data):
+def clean_artists(artists):
     pass
 
+def clean_albums(albums):
+    # for duplicates from collaborations:
+    albums.drop_duplicates(ignore_index=True, subset=["album_id"], inplace=True) 
+    # for duplicates of the same album, different "spotify version":
+    albums.drop_duplicates(ignore_index=True, subset=["album_name", "total_tracks", "artist_id"], inplace=True)
+    
+    # remove all "appears_on" albums.
+    filter = albums["album_group"] != "appears_on"
+    albums = albums[filter]
+    albums.sort_index(ignore_index=True, inplace=True)
+    return albums
 
-def clean_albums(data):
-    pass
+def clean_tracks(tracks, albums):
+    tracks.drop_duplicates(ignore_index=True, inplace=True)
+    
+    # remove all songs that aren't from the updated DataFrame of albums.
+    # (this makes sure that all "appears_on" tracks are removed.)
+    filter = tracks["album_id"].isin(albums["album_id"])
+    tracks = tracks[filter]
+    tracks.sort_index(ignore_index=True, inplace=True)
+    return tracks
 
-
-def clean_tracks(data):
-    pass
-
-
-def clean_track_features(data):
-    pass
+def clean_track_features(track_features, tracks):
+    track_features.drop_duplicates(ignore_index=True, inplace=True)
+    
+    # only include track_features for tracks that exist in the track DataFrame.
+    filter = track_features["track_id"].isin(tracks["track_id"])
+    track_features = track_features[filter]
+    track_features.sort_index(ignore_index=True, inplace=True)
+    return track_features
     
 
 
 
-
+#################################################
 def main():
-    pass # run the main method of extract.py to get all the data so that you can transform it.
+    artists = clean_artists(artists)
+    albums = clean_albums(albums)
+    tracks = clean_tracks(tracks, albums)
+    track_features = clean_track_features(track_features, tracks)
 
 
 if __name__ == "__main__":
