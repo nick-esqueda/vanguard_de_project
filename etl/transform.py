79b24e6a04f86382d8aea936cee77453ea11aa61
nick-esqueda <nick_esqueda@outlook.com>
2022-09-26 18:25:48 -0500

    added dropna() logic in transform functions

--- etl/transform.py
+++ etl/transform.py
@@ -3,17 +3,13 @@ from extract import *
 from utils.artist_urls import ARTIST_URLS
 
 
-# # READ IN DATA
-# artists = pd.read_csv("data/artists.csv")
-# albums = pd.read_csv("data/albums.csv")
-# tracks = pd.read_csv("data/tracks.csv")
-# track_features = pd.read_csv("data/track_features.csv")
-
-
 def clean_artists(artists):
-    pass
+    artists.dropna(inplace=True)
+    return artists
 
 def clean_albums(albums):
+    albums.dropna(subset=["album_id", "album_name", "external_url", "album_uri", "artist_id"], inplace=True)
+    
     # for duplicates from collaborations:
     albums.drop_duplicates(ignore_index=True, subset=["album_id"], inplace=True) 
     # for duplicates of the same album, but different "spotify version":
@@ -26,6 +22,7 @@ def clean_albums(albums):
     return albums
 
 def clean_tracks(tracks, albums):
+    tracks.dropna(subset=["track_id", "song_name", "external_url", "song_uri", "album_id"], inplace=True)
     tracks.drop_duplicates(ignore_index=True, inplace=True)
     
     # remove all songs that aren't from the updated DataFrame of albums.
@@ -36,6 +33,7 @@ def clean_tracks(tracks, albums):
     return tracks
 
 def clean_track_features(track_features, tracks):
+    track_features.dropna(inplace=True)
     track_features.drop_duplicates(ignore_index=True, inplace=True)
     
     # only include track_features for tracks that exist in the track DataFrame.
@@ -49,19 +47,30 @@ def clean_track_features(track_features, tracks):
 
 #################################################
 def main():
-    # fetch all of the data first.
-    artists = extract_artists(ARTIST_URLS)
-    albums = extract_artists_albums(ARTIST_URLS)
-    album_ids = [album["album_id"] for album in albums]
-    tracks = extract_albums_tracks(album_ids)
-    track_ids = [track["track_id"] for track in tracks]
-    track_features = extract_track_features(track_ids)
+    # # fetch all of the data first.
+    # artists = extract_artists(ARTIST_URLS)
+    # albums = extract_artists_albums(ARTIST_URLS)
+    # album_ids = [album["album_id"] for album in albums]
+    # tracks = extract_albums_tracks(album_ids)
+    # track_ids = [track["track_id"] for track in tracks]
+    # track_features = extract_track_features(track_ids)
+    
+    # OPTIONAL: use data from .csv instead of fetching everything again.
+    artists = pd.read_csv("data/artists.csv")
+    albums = pd.read_csv("data/albums.csv")
+    tracks = pd.read_csv("data/tracks.csv")
+    track_features = pd.read_csv("data/track_features.csv")
     
     # clean the extracted data.
-    artists = clean_artists(artists)
-    albums = clean_albums(albums)
-    tracks = clean_tracks(tracks, albums)
-    track_features = clean_track_features(track_features, tracks)
+    artists = clean_artists(pd.DataFrame(artists))
+    albums = clean_albums(pd.DataFrame(albums))
+    tracks = clean_tracks(pd.DataFrame(tracks), pd.DataFrame(albums))
+    track_features = clean_track_features(pd.DataFrame(track_features), pd.DataFrame(tracks))
+    
+    print(artists)
+    print(albums)
+    print(tracks)
+    print(track_features)
 
 
 if __name__ == "__main__":
