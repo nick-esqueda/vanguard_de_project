b775fa3503a246edb40b7b673414d67c4ece4276
nick-esqueda <nick_esqueda@outlook.com>
2022-09-27 11:55:20 -0500

    WIP creating db connection class to abstract out sqlite connection logic

---
+++ etl/utils/db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+
+class DB:
+    def __init__(self):
+        self.conn = sqlite3.connect("etl/data/spotify.db")
+        self.curs = self.conn.cursor()
+        
+    def execute(self, query):
+        with self.conn:
+            self.curs.execute(query)
+        
+    def close(self): 
+        self.conn.close()
