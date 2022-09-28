2086675720506df0ddbe9e1311ea3a67b8591041
nick-esqueda <nick_esqueda@outlook.com>
2022-09-27 23:26:06 -0500

    refactored clean_tracks() to remove the single versions of songs that are on albums

--- app/analytics.py
+++ app/analytics.py
@@ -59,8 +59,8 @@ def test_custom_views(db: DB) -> None:
 def main():
     db = DB()
     create_views(db)
-    # test_prompt_views(db)
-    test_custom_views(db)
+    test_prompt_views(db)
+    # test_custom_views(db)
 
 
 if __name__ == "__main__":
--- app/transform.py
+++ app/transform.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from app.utils.io import read_all_from_csv
+from utils import read_all_from_csv
 import extract
 
 
@@ -22,14 +22,26 @@ def clean_albums(albums: pd.DataFrame) -> pd.DataFrame:
     albums.sort_index(ignore_index=True, inplace=True)
     return albums
 
-def clean_tracks(tracks: pd.DataFrame, albums: pd.DataFrame) -> pd.DataFrame:
+def clean_tracks(tracks: pd.DataFrame, albums: pd.DataFrame, artists: pd.DataFrame) -> pd.DataFrame:
     tracks.dropna(subset=["track_id", "song_name", "external_url", "song_uri", "album_id"], inplace=True)
     tracks.drop_duplicates(ignore_index=True, inplace=True)
     
     # remove all songs that aren't from the updated DataFrame of albums.
-    # (this makes sure that all "appears_on" tracks are removed.)
+    # this makes sure that all "appears_on" tracks are removed.
     filter = tracks["album_id"].isin(albums["album_id"])
     tracks = tracks[filter]
+    
+    # find all single versions of album tracks and remove them.
+    merged = tracks.merge(albums, on="album_id").merge(artists, on="artist_id")
+    merged.sort_values(["album_group", "artist_name"], inplace=True) # to make sure the "album" type comes first.
+    
+    duped = (merged.duplicated(subset=["artist_id", "song_name", "duration_ms"])) & (merged["album_group"] == "single")
+    keep_ids = merged[~duped]["track_id"] # this holds all of the track_ids that you want to keep.
+    
+    # NOTE: keep the "tracks" df and "merged" df separate since "merged" is sorted differently.
+    filter = tracks["track_id"].isin(keep_ids)
+    tracks = tracks[filter]
+    
     tracks.sort_index(ignore_index=True, inplace=True)
     return tracks
 
@@ -58,16 +70,16 @@ def print_transformations(artists, albums, tracks, track_features):
 ##########################################################################
 def main():
     # # fetch all of the data first.
-    # artists, albums, tracks, track_features = extract.main()
+    # artists, albums, tracks, track_features = (pd.DataFrame(data) for data in extract.main())
     
     # OPTIONAL: use data from .csv instead of extracting everything again.
     artists, albums, tracks, track_features = read_all_from_csv()
     
     # clean the extracted data.
-    artists = clean_artists(pd.DataFrame(artists))
-    albums = clean_albums(pd.DataFrame(albums))
-    tracks = clean_tracks(pd.DataFrame(tracks), pd.DataFrame(albums))
-    track_features = clean_track_features(pd.DataFrame(track_features), pd.DataFrame(tracks))
+    artists = clean_artists(artists)
+    albums = clean_albums(albums)
+    tracks = clean_tracks(tracks, albums, artists)
+    track_features = clean_track_features(track_features, tracks)
     
     # print_transformations(artists, albums, tracks, track_features)
     
