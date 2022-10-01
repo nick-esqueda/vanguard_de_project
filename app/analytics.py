3bc529f85fb9e1297662a82b0f82e1b02727e835
nick-esqueda <nick_esqueda@outlook.com>
2022-09-30 19:18:43 -0500

    moved all SQL constants to an iterable for importing to improve cleanliness

--- app/analytics.py
+++ app/analytics.py
@@ -1,28 +1,21 @@
 import pandas as pd
-from .utils import DB
-from .utils import V_ARTIST_TOP_SONGS_BY_DURATION, V_TOP_ARTISTS_BY_FOLLOWERS
-from .utils import V_ARTIST_TOP_SONGS_BY_TEMPO, V_ARTIST_OVERVIEW
-from .utils import V_POPULAR_ARTIST_FEATURES, V_GENRE_FEATURES, V_GENRE_RELEASE_PATTERNS
+from .utils import DB, db_view_names, db_view_queries
 
 
 # VIEWS #########################################
 def create_views(db: DB) -> None:
-    # drop the views if they had already been created. (this is to "start fresh" every time.)
-    db.execute("DROP VIEW IF EXISTS V_artist_top_songs_by_duration;")
-    db.execute("DROP VIEW IF EXISTS V_top_artists_by_followers;")
-    db.execute("DROP VIEW IF EXISTS V_artist_top_songs_by_tempo;")
-    db.execute("DROP VIEW IF EXISTS V_artist_overview;")
-    db.execute("DROP VIEW IF EXISTS V_popular_artist_features;")
-    db.execute("DROP VIEW IF EXISTS V_genre_features;")
-    db.execute("DROP VIEW IF EXISTS V_genre_release_patterns;")
-    # (re)create the views in the database.
-    db.execute(V_ARTIST_TOP_SONGS_BY_DURATION)
-    db.execute(V_TOP_ARTISTS_BY_FOLLOWERS)
-    db.execute(V_ARTIST_TOP_SONGS_BY_TEMPO)
-    db.execute(V_ARTIST_OVERVIEW)
-    db.execute(V_POPULAR_ARTIST_FEATURES)
-    db.execute(V_GENRE_FEATURES)
-    db.execute(V_GENRE_RELEASE_PATTERNS)
+    """
+    this function will create all of the pre-determined set of views, using the
+    SQL queries provided by db_views.py.
+    
+    NOTE: all views are dropped before executing the "CREATE VIEW" queries again.
+    this is to pick up on any future changes to the views.
+    """
+    for v_name in db_view_names:
+        db.execute(f"DROP VIEW IF EXISTS {v_name};")
+        
+    for v_query in db_view_queries:
+        db.execute(v_query)
     
 def test_view(db: DB, view_name: str) -> None:
     db.execute(view_name)
--- app/load.py
+++ app/load.py
@@ -1,23 +1,29 @@
 import pandas as pd
-from .utils import DB
-from .utils import CREATE_ARTISTS, CREATE_ALBUMS, CREATE_TRACKS, CREATE_TRACK_FEATURES
+from .utils import DB, db_table_names, db_table_queries
 
 
 # CREATE TABLES #################################
 def create_tables(db: DB) -> None:
-    # drop the tables if they had already been created. (this is to "start fresh" every time.)
-    db.execute("DROP TABLE IF EXISTS artists")
-    db.execute("DROP TABLE IF EXISTS albums")
-    db.execute("DROP TABLE IF EXISTS tracks")
-    db.execute("DROP TABLE IF EXISTS track_features")
-    # (re)create the tables in the database.
-    db.execute(CREATE_ARTISTS)
-    db.execute(CREATE_ALBUMS)
-    db.execute(CREATE_TRACKS)
-    db.execute(CREATE_TRACK_FEATURES)
+    """
+    this function will create all of the pre-determined set of DB tables, using the
+    SQL queries provided by db_tables.py.
+    
+    NOTE: all tables are dropped before executing the "CREATE TABLE" queries again.
+    this is to pick up on any future changes to the tables.
+    """
+    for t_name in db_table_names:
+        db.execute(f"DROP TABLE IF EXISTS {t_name}")
+    
+    for t_query in db_table_queries:
+        db.execute(t_query)
         
 # INSERTING DATA ################################
 def load_data(data: pd.DataFrame, tablename: str, db: DB) -> None:
+    """
+    this function inserts the given data into the specified table using the
+    DB connection object, and prints a confirmation message to the terminal
+    after insertion.
+    """
     data.to_sql(tablename, db.conn, if_exists="replace", index=False)
     print(f"Finished loading data into table: {tablename}")
 
